158bb14670f1415079d75173e795bd9721ee3273
Khalil El Kaaki <khalil.el.kaaki@gmail.com>
2025-08-06 11:05:49 +0300

    Update CardService.java

--- src/main/java/com/example/cms/service/CardService.java
+++ src/main/java/com/example/cms/service/CardService.java
@@ -1,4 +1,14 @@
 package com.example.cms.service;
 
+import com.example.cms.repository.CardRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class CardService {
+
+    public final CardRepository cardRepository;
+
+    public CardService(CardRepository cardRepository) {
+        this.cardRepository = cardRepository;
+    }
 }
